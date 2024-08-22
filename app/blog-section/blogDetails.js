562b15020f8f0bbb4125cfa9be319036f382892a
work4inventions <work4inventions@gmail.com>
2024-08-22 09:12:00 +0530

    Second comm

--- app/blog-section/blogDetails.js
+++ app/blog-section/blogDetails.js
@@ -5,7 +5,7 @@ export const blogDetails = [
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1715345723909/c71d9691-fe4c-4302-b1cb-d9dca77a99b5.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
     date: "MAY 2024",
     url:
-      "https://blog.victorwilliams.me/how-to-create-exit-animations-with-framer-motion",
+      "https://www.framer.com/motion/animate-presence/",
     available: true
   },
   {
@@ -13,7 +13,7 @@ export const blogDetails = [
     image:
       "https://github.com/victorcodess/folio-v1/assets/84178696/05656547-3206-42af-b081-83247f9a1063",
     date: "MAY 2024",
-    url: "https://blog.victorwilliams.me/importance-of-collaboration-in-design",
+    url: "https://www.wix.com/studio/blog/collaboration-design#:~:text=Collaboration%20unifies%20the%20team%20around,together%20on%20a%20problem%20systematically.",
     available: true
   },
   {
@@ -23,7 +23,7 @@ export const blogDetails = [
       "https://github.com/victorcodess/folio-v1/assets/84178696/c2693469-e5c6-43ad-a857-693bbbfe7b9a",
     date: "APR 2024",
     url:
-      "https://blog.victorwilliams.me/how-to-add-google-maps-to-react-app-visgl",
+      "https://medium.com/@reactcompany01/integrating-google-maps-with-react-applications-a-comprehensive-guide-97f3c48e1093",
     available: true
   }
 ]
